ec0010b3375b792c1e6bcaf88d2049e1874ccf5d
damian <adamodamian6@gmail.com>
2023-04-01 21:00:54 -0300

    docker

---
+++ msvc-autor/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO `autores` (nombre) VALUE ('autor 1');
+INSERT INTO `autores` (nombre) VALUE ('autor 2');
+INSERT INTO `autores` (nombre) VALUE ('autor 3');
+INSERT INTO `autores` (nombre) VALUE ('autor 4');
---
+++ msvc-libro/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO `libros` (nombre,ejemplares,autorId) VALUE ('libro 1',10,2);
+INSERT INTO `libros` (nombre,ejemplares,autorId) VALUE ('libro 2',10,2);
+INSERT INTO `libros` (nombre,ejemplares,autorId) VALUE ('libro 3',10,2);
+INSERT INTO `libros` (nombre,ejemplares,autorId) VALUE ('libro 4',10,2);
